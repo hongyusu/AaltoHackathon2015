b08b586532dc111641e61310366ca6199f905d48
hongyusu <hongyu.su.uh@gmail.com>
2015-04-24 23:52:00 +0300

    revision on track basic information extraction

--- TrackSelection/make_kml.py
+++ TrackSelection/make_kml.py
@@ -10,9 +10,13 @@ t2 = open('place_tail.txt').read()
 
 kmlfile=''
 kmlfile+=h1
+ind = 0
 for line in open('file_first_gps.txt'):
+  ind = ind + 1
   words = line.strip().split(' ')
   kmlfile=kmlfile+h2+words[1]+','+words[2]+t2
+  if ind == 1000:
+    break
 
 kmlfile = kmlfile + t1
 print kmlfile
--- TrackSelection/track_basic_information_extraction.py
+++ TrackSelection/track_basic_information_extraction.py
@@ -8,13 +8,16 @@ endomondodir = '../Endomondo/'
 def get_track_position():
   for file in os.listdir(endomondodir):
     for line in open(endomondodir+file):
+      if not len(re.findall('sport',line)) == 0:
+        words = line.strip().split(': ')
+        sporttype = words[1]
       if not len(re.findall('lng',line)) == 0:
         line = re.sub('.*lng": ','', line)
         line = re.sub(',.*: ',' ',line)
         words = line.strip().split(' ')
         lng = words[0]
         lat = words[1]
-        print file,lng,lat
+        print file,lng,lat,sporttype
         break
   pass
 
