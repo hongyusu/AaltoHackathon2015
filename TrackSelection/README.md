10aea6c2981ac573f0c33314a3e91100f51ae53a
hongyusu <hongyu.su.uh@gmail.com>
2015-04-24 21:49:23 +0300

    now extract track information and first gps location

---
+++ TrackSelection/README.md
@@ -0,0 +1,7 @@
+
+
+**select tracks**
+==
+
+  track_selection.py		parse track information and extract track names and first gps location
+  file_first_gps.txt		format: tracks" "lng" "lat
--- TrackSelection/track_selection.py
+++ TrackSelection/track_selection.py
@@ -9,11 +9,14 @@ def get_track_position():
   for file in os.listdir(endomondodir):
     for line in open(endomondodir+file):
       if not len(re.findall('lng',line)) == 0:
-        re.sub('lng":')
-        print line
-    break
+        line = re.sub('.*lng": ','', line)
+        line = re.sub(',.*: ',' ',line)
+        words = line.strip().split(' ')
+        lng = words[0]
+        lat = words[1]
+        print file,lng,lat
+        break
   pass
 
 if __name__ == '__main__':
-  print "hello world"
   get_track_position()
