1365b4c1e05a256445d66b22254dc1930230d3e9
hongyusu <hongyu.su.uh@gmail.com>
2015-04-25 10:04:20 +0300

    add distance function

--- TrackSelection/README.md
+++ TrackSelection/README.md
@@ -5,3 +5,10 @@
 
   track_selection.py		parse track information and extract track names and first gps location
   file_first_gps.txt		format: tracks" "lng" "lat
+
+
+
+**Coordinate**
+==
+  raw data 			(x,y) 		(lng,lat)
+  city data 		(x,y) 		(lat,lng)
--- TrackSelection/track_selection.py
+++ TrackSelection/track_selection.py
@@ -6,9 +6,29 @@
 # 51.7, -0.2
 
 
-for line in open('file_first_gps.txt'):
-  words = line.strip().split(' ')
-  y = eval(words[1])
-  x = eval(words[2])
-  if x>=51.3 and x<=51.7 and y>=-0.5 and y<=0.2:
-    print line,
+# city data is (lat,lng)
+# raw data is (lng,lat)
+
+import os
+
+helsinki_gps = (+60.16660,+24.88330)
+london_gps = (+51.50000,-0.11670)
+
+
+
+delta = 0.00001*1000*30
+
+for city in ['helsinki','london']:
+  if city == 'helsinki':
+    (x0,y0) = helsinki_gps
+  if city == 'london':
+    (x0,y0) = london_gps
+  fout = open('file_first_gps_%s.txt' % city,'w')
+  for line in open('file_first_gps.txt'):
+    words = line.strip().split(' ')
+    y = eval(words[1])
+    x = eval(words[2])
+    if x>=x0-delta/2 and x<=x0+delta/2 and y>=y0-delta/2 and y<=y0+delta/2:
+      fout.write(line)
+    fout.close()
+    os.system('python compute_kml_from_track_basic_information.py file_first_gps_%s.txt file_first_gps_%s.kml' % (city,city))
