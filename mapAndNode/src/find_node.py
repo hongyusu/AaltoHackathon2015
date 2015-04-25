dd2d92b20065cee959652f0edb0ca4e677d6ff5a
czli <julia.lcx@gmail.com>
2015-04-25 11:36:48 +0300

    node info and map info generated

---
+++ mapAndNode/src/find_node.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from data_preprocess import nodeInfoPath, mapInfoPath, center
+
+NodeList = {}
+
+def find_node(lng, lat):
+        searchKey = "%s,%s"%(center(lng), center(lat))
+
+        #read the existing cener points
+        with open
+
+print nodeInfoPath
+print mapInfoPath
