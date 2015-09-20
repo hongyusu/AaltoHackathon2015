9bb122830ccc2bb115ee8508dcc0d30962161b78
hongyusu <hongyu.su.uh@gmail.com>
2015-09-20 11:11:15 +0300

    add wrapper function to run GNFM

---
+++ Recommender/SpatialRecommender/run_SR.m
@@ -0,0 +1,21 @@
+
+
+function run_SR
+% wrapper function for running Spatial Recommender (SR)
+
+    fea = randn(1000,2000);
+    fea = fea - min(min(fea));
+
+    %NMF learning
+    options = [];
+    options.maxIter = 100;
+    options.alpha = 0;
+    %when alpha = 0, GNMF boils down to the ordinary NMF.
+    nClass = 20;
+    [U,V] = GNMF(fea',nClass,[],options); %'
+
+    size(fea)
+    size(U)
+    size(V)
+    
+end
